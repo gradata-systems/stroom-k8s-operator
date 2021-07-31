7b57ec2e13f1620f62c46b2573d84c955bda1667
Peter Kimberley <peter@gradata.com.au>
2021-08-01 00:04:11 +1000

    Generate ConfigMap if no override is specified

--- controllers/stroomcluster_controller.go
+++ controllers/stroomcluster_controller.go
@@ -126,7 +126,7 @@ func (r *StroomClusterReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	}
 
 	// Create a ConfigMap containing Stroom configuration and lifecycle scripts
-	if !stroomCluster.Spec.ConfigMapRef.IsZero() {
+	if stroomCluster.Spec.ConfigMapRef.IsZero() {
 		foundConfigMap := corev1.ConfigMap{}
 		result, err = r.getOrCreateObject(ctx, stroomCluster.GetStaticContentConfigMapName(), stroomCluster.Namespace, "ConfigMap", &foundConfigMap, func() error {
 			if files, err := StaticFiles.ReadDir("static_content"); err != nil {
