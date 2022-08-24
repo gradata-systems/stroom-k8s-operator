07ce6bfffbd00ec3a0580542cc0e662b3715c6ee
Peter Kimberley <peter@gradata.com.au>
2022-08-24 21:37:55 +1000

    Add helm charts

---
+++ deploy/helm-substitute-vars.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version="$1"
+
+chart_yaml='./charts/stroom-operator/Chart.yaml'
+operator_yaml='./charts/stroom-operator/templates/operator.yaml'
+
+sed -i -E "s/appVersion: \"(.*)\"/appVersion: \"$version\"/" $chart_yaml
+
+registry="{{ if .Values.registry }}{{ printf \"%s\/\" .Values.registry }}{{ end }}"
+sed -i -E "s/(image: .+):HELM_IMAGE_TAG/\1:{{ .Values.image.tag | default .Chart.AppVersion }}/" $operator_yaml
+sed -i -E "s/(image): (.+):/\1: $registry{{ \"\2\" }}:/" $operator_yaml
+
+sed -i 's/HELM_LABELS: ""/{{ include "stroom-operator.labels" . | nindent 4 }}/' $operator_yaml
+sed -i 's/HELM_IMAGE_PULL_POLICY/{{ .Values.image.pullPolicy }}/' $operator_yaml
+sed -i 's/HELM_RESOURCES/{{ toYaml .Values.resources | nindent 10 }}/' $operator_yaml
+sed -i 's/HELM_NODE_SELECTOR/{{ toYaml .Values.nodeSelector | nindent 8 }}/' $operator_yaml
+sed -i 's/HELM_TOLERATIONS/{{ toYaml .Values.tolerations | nindent 8 }}/' $operator_yaml
+sed -i 's/HELM_AFFINITY/{{ toYaml .Values.affinity | nindent 8 }}/' $operator_yaml
