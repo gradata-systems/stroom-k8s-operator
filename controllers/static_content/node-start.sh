b3f3e429a02a01bf3541f0ded11ab222996ddc21
Peter Kimberley <peter@gradata.com.au>
2023-12-27 01:45:15 +1100

    Use auth token API endpoint

--- controllers/static_content/node-start.sh
+++ controllers/static_content/node-start.sh
@@ -5,15 +5,11 @@ source /stroom/scripts/utils.sh
 
 log_file='/stroom/logs/k8s/node-start.log'
 
-function log() {
-  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $log_file
-}
-
 mkdir -p "$(dirname $log_file)"
 
 # Enable the node
 call_api node/v1/enabled/"${STROOM_NODE}" -X PUT -d true
-log "Node ${STROOM_NODE} enabled"
+log "Node ${STROOM_NODE} enabled" $log_file
 
 # Enable all node tasks (except if this is a dedicated UI nodes)
 enabled='true'
@@ -22,7 +18,7 @@ if [[ "${STROOM_NODE_ROLE}" == 'Frontend' ]]; then
 fi
 call_api job/v1/setJobsEnabled/"${STROOM_NODE}" -X PUT -d "{ \"enabled\": $enabled }"
 if [[ $enabled == 'true' ]]; then
-  log "Node ${STROOM_NODE} jobs enabled"
+  log "Node ${STROOM_NODE} jobs enabled" $log_file
 else
-  log "Node ${STROOM_NODE} jobs disabled (as this is a dedicated UI node)"
+  log "Node ${STROOM_NODE} jobs disabled (as this is a dedicated UI node)" $log_file
 fi
