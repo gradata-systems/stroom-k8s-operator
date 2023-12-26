c177c8003282059b0517d728680340031b7247e2
Peter Kimberley <peter@gradata.com.au>
2023-12-27 01:47:11 +1100

    Remove disused constants

--- api/v1/constants.go
+++ api/v1/constants.go
@@ -3,16 +3,7 @@ package v1
 const (
 	StroomClusterFinalizerName = "stroomcluster.finalizers.stroom.gchq.github.io"
 	WaitNodeTasksFinalizerName = "waitnodetasks.finalizers.stroom.gchq.github.io"
-	StroomOperatorUserId       = "K8S_OPERATOR"
 
 	// SecretFileMode is the file mode to use for Secret volume mounts
 	SecretFileMode int32 = 0400
-)
-
-type UserPermission string
-
-const (
-	UserPermissionManageNodes UserPermission = "Manage Nodes"
-	UserPermissionManageJobs  UserPermission = "Manage Jobs"
-	UserPermissionManageTasks UserPermission = "Manage Tasks"
 )
